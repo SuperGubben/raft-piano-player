b5f86d764de66a0fbd7fb6f8a0b4fffa378ba214
Oscar <oscariv76@gmail.com>
2024-08-04 21:28:18 +0200

    Seperated hold_note and short_note

--- play_note.py
+++ play_note.py
@@ -3,7 +3,7 @@ import time as t
 
 kb = Controller()
 
-def play_note(key: str, length: int = 600, tone: int = 0, instant: bool = False):
+def hold_note(key: str, length: int = 600, tone: int = 0):
     print(f"pressing {key} for {length}ms with tone {tone}")
 
     if tone == -1:
@@ -13,13 +13,25 @@ def play_note(key: str, length: int = 600, tone: int = 0, instant: bool = False)
 
     kb.press(key)
 
-    if not instant:
-        t.sleep(length/1000)
+    t.sleep(length/1000)
 
     kb.release(key)
 
     kb.release(Key.space)
     kb.release(Key.shift)
 
-    if instant:
-        t.sleep(length/1000)
+def short_note(key: str, length: int = 600, tone: int = 0): #key: the key you want to play. length: how long you want to wait after playing another note. tone: high or low octave
+    print(f"pressing {key} for {length}ms with tone {tone}")
+
+    if tone == -1:
+        kb.press(Key.space)
+    if tone == 1:
+        kb.press(Key.shift)
+
+    kb.press(key)
+    kb.release(key)
+
+    kb.release(Key.space)
+    kb.release(Key.shift)
+
+    t.sleep(length/1000)
